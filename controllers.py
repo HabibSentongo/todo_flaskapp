f4dec5ca7ce88311ba80c6979aae0126c08cc304
habibsentongo <habibsentongo@gmail.com>
2019-12-23 19:39:28 +0300

    Change table creation to use Flask-Migrate
    - import migrate class from flask-migrate
    - create migrate object initialised with app and db instances
    - create flask db repo
    - make db migrations
    - effect migrations to the db
    
    [Delivers #170427613]

--- controllers.py
+++ controllers.py
@@ -1,5 +1,6 @@
 from flask import render_template, request, redirect, url_for, jsonify
 from models import ToDo, db
+import sys
 
 class AllMethods:
 
@@ -7,10 +8,18 @@ class AllMethods:
         return render_template('index.html', data = ToDo.query.all())
 
     def create(self):
-        description = request.get_json()['description']
-        newToDo = ToDo(description = description)
-        db.session.add(newToDo)
-        db.session.commit()
-        return jsonify({
-            'description' : description
-        })
+        error = False
+        body = {}
+        try:
+            description = request.get_json()['description']
+            newToDo = ToDo(description = description)
+            db.session.add(newToDo)
+            db.session.commit()
+            body = {'description' : newToDo.description}
+        except:
+            error = True
+            db.session.rollback()
+            print(sys.exec_info())
+        finally:
+            db.session.close()
+        return jsonify(body)
--- models.py
+++ models.py
@@ -2,19 +2,19 @@ from app import app
 import os
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
 class ToDo(db.Model):
     __tablename__ = 'todo'
     id = db.Column(db.Integer, primary_key = True)
     description = db.Column(db.String, nullable = False)
     due = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
-
+    completed = db.Column(db.Boolean, nullable = False, default = False)
     def __repr__():
         return f'<ID: {self.id}, Description: {self.description}, Due: {self.due}>'
-
-db.create_all()
